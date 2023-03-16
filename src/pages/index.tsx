832aaeb537037733a21b67ba7a722f2a48815011
Ventura Junior <jvmjunior@yahoo.com>
2023-03-16 04:04:47 -0300

    Install and configure Stitches library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(1.2)',
+  },
+})
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <Button>
+        <span>teste</span>
+        Botão
+      </Button>
+      <h1>Hello world</h1>
+    </>
+  )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  theme,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+    },
+  },
+})
