9528aa70c4522f2510514499933d4197205cace4
Ventura Junior <jvmjunior@yahoo.com>
2023-03-16 02:34:22 -0300

    Application's routes creation

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+
+export default function Product() {
+  const {query} = useRouter()
+  return (
+    <h1>This is the product page: {JSON.stringify(query)}</h1>
+  )
+}
