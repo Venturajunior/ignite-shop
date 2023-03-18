ff32d715d67fe6997ca5db489b1532cf47083c81
Ventura Junior <jvmjunior@yahoo.com>
2023-03-18 04:05:20 -0300

    Add slider for products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,52 @@
 import { HomeContainer, Product } from '../styles/pages/home'
 import Image from 'next/image'
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
 import camisa1 from '../assets/camisas/1.png'
 import camisa2 from '../assets/camisas/2.png'
 import camisa3 from '../assets/camisas/3.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
     <>
-      <HomeContainer>
-        <Product>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        <Product className="keen-slider__slide">
           <Image src={camisa1} width={520} height={480} alt="" />
           <footer>
-            <strong>Camiseta X</strong>
+            <strong>Camiseta ABC</strong>
             <span>R$ 79,90</span>
           </footer>
         </Product>
 
-        <Product>
+        <Product className="keen-slider__slide">
           <Image src={camisa2} width={520} height={480} alt="" />
           <footer>
-            <strong>Camiseta X</strong>
+            <strong>Camiseta XPTO</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+          <Image src={camisa3} width={520} height={480} alt="" />
+          <footer>
+            <strong>Camiseta QWE</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+          <Image src={camisa2} width={520} height={480} alt="" />
+          <footer>
+            <strong>Camiseta AZER</strong>
             <span>R$ 79,90</span>
           </footer>
         </Product>
